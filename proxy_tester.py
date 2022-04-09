97863f956066715bc01e6e4b8e459cb5e6b37e78
Jiong-BornToCOOK <33740332+lsy13522514671@users.noreply.github.com>
2022-04-09 19:24:04 -0400

    naive proxy tester

--- proxy_tester.py
+++ proxy_tester.py
@@ -1,29 +1,158 @@
-from urllib import response
 import requests
-import asyncio
 from requests.auth import HTTPProxyAuth
+import time
+import os
+import glob
+import validators
+import sys
 
-# proxy_file = open('proxy.txt', 'r')
-# proxies = proxy_file.readlines()
-# proxy_dic = {}
 
-url = "http://www.google.com"
+def format_proxy(proxy):
+    proxy_format = dict.fromkeys(["ip", "username", "password"])
+    p_list = proxy.split(":")
+    if(len(p_list) != 4):
+        return False
+    proxy_format['ip'] = p_list[0] + ":" + p_list[1]
+    proxy_format['username'] = p_list[2]
+    proxy_format['password'] = p_list[3]
+    return proxy_format
 
 
-def ping():
+def ping_proxy(proxy_format, url):
+    info_format = dict.fromkeys(["status", "code", "latency"])
     proxy = {
-        'http': '207.202.130.186:5696',
-        'https': '207.202.130.186:5696'
+        'http': proxy_format['ip'],
+        'https': proxy_format['ip']
     }
 
-    auth = HTTPProxyAuth('Na2wCBfHX0', '3Kofo4VRwr')
+    auth = HTTPProxyAuth(proxy_format['username'], proxy_format['password'])
     try:
+        start_time = time.time()
         res = requests.get(url, proxies=proxy, auth=auth, timeout=5)
-        if res.status_code //100 == 2:
-            print("good status: ", res.status_code)
+        end_time = time.time()
+        if res.status_code // 100 == 2:
+            info_format['status'] = True
+            info_format['latency'] = str(
+                int(round((end_time - start_time), 2) * 1000)) + "ms"
         else:
-            print("error with code: ", res.status_code)
+            info_format['status'] = False
+            info_format['latency'] = "none"
+        info_format['code'] = res.status_code
+        return info_format
     except requests.exceptions.RequestException:
-        print("proxy error")
+        info_format['status'] = False
+        info_format['code'] = "proxy error"
+        info_format['latency'] = "none"
+        return info_format
 
-ping()
+
+def read_proxy_to_lib(file_path, url):
+    proxy_list = []
+    proxy_file = open(file_path, "r")
+    proxies = proxy_file.readlines()
+    line_length = len(proxies)
+    line_num = 0
+    for proxy in proxies:
+        if(line_num < line_length-1):
+            cleaned_proxy = proxy[:-1]
+            line_num += 1
+        else:
+            cleaned_proxy = proxy
+        r = format_proxy(cleaned_proxy)
+        if(r):
+            proxy_info = dict.fromkeys(["proxy"])
+            res = ping_proxy(r, url)
+            proxy_info['proxy'] = cleaned_proxy
+            proxy_info.update(res)
+            proxy_list.append(proxy_info)
+        else:
+            return False
+    return proxy_list
+
+
+def shuffle_good_proxy(list, file_name):
+    os.chdir("./shuffled")
+    file = open("shuffled_"+file_name, "w")
+    for proxy in list:
+        if(proxy['status']):
+            file.write(proxy['proxy'])
+            file.write("\n")
+    file.close()
+    os.chdir("../")
+
+
+def write_proxy_status(list, file_name):
+    os.chdir("./status")
+    file = open("status_"+file_name, "w")
+    for proxy in list:
+        file.write(proxy['proxy'])
+        file.write("----")
+        if(proxy['status']):
+            file.write("good")
+        else:
+            file.write("bad")
+            file.write("-error-code:" + str(proxy['code']))
+        file.write("----")
+        file.write(proxy['latency'])
+        file.write("\n")
+    file.close()
+    os.chdir("../")
+
+
+def main():
+    folders = ["shuffled", "status"]
+    for folder in folders:
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+    proxy_files = glob.glob('*.txt')
+
+    while True:
+        url = str(input("Please Give An URL To Test:"))
+        if not validators.url(url):
+            print("Invalid URL! Please Try Again!\n")
+        else:
+            break
+
+    while True:
+        option = int(input(
+            "Please Select An Option Below: \n 1. Test All Proxy Lists \n 2. Test A Specific Proxy List \n 3. Reset URL \n 4. Quit Application \n Your Option: \n"))
+        if(option == 1):
+            for proxy_file in proxy_files:
+                proxy_list = read_proxy_to_lib(proxy_file, url)
+                if proxy_list:
+                    shuffle_good_proxy(proxy_list, proxy_file)
+                    write_proxy_status(proxy_list, proxy_file)
+                else:
+                    print("Wrong Proxy List Format: "+proxy_file+"\n")
+        elif(option == 2):
+            file_name=str(input("Please Enter The File Name With Suffix (For Example, file.txt): \n"))
+            if(proxy_files.count(file_name)>0):
+                proxy_list = read_proxy_to_lib(file_name, url)
+                shuffle_good_proxy(proxy_list, file_name)
+                write_proxy_status(proxy_list, file_name)
+            else:
+                print("File Does not Exist, Please Try Again! \n")
+        elif(option == 3):
+            print("\n")
+            main()
+        elif(option == 4):
+            print("\n")
+            print("Successfully Quitted!\n")
+            sys.exit(0)
+        else:
+            print("Invalid Input! Please Try Again!\n")
+        print("\n")
+
+
+if __name__ == '__main__':
+    main()
+# if __name__ == '__main__':
+#     url = "http://www.google.com"
+#     folders = ["shuffled", "status"]
+#     for folder in folders:
+#         if not os.path.isdir(folder):
+#             os.mkdir(folder)
+#     proxy_list = read_proxy_to_lib('good.txt', url)
+#     print(proxy_list)
+#     shuffle_good_proxy(proxy_list, 'good.txt')
+#     write_proxy_status(proxy_list, 'good.txt')
